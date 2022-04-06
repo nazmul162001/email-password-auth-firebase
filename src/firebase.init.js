5472d7aa24960256fb4120bbc461e1343f97833e
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-07 01:23:48 +0600

    firebase email & pasword auth setUp

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKYnf2A8Q3f9o02PxrIylP5eOYYtUKngU",
+  authDomain: "email-password-auth-4ab3f.firebaseapp.com",
+  projectId: "email-password-auth-4ab3f",
+  storageBucket: "email-password-auth-4ab3f.appspot.com",
+  messagingSenderId: "648848144624",
+  appId: "1:648848144624:web:c0ddd4606a9d1666162a35"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
